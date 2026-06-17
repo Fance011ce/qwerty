32c40903e90c5c8666d5301e8c183c319b81450b
Fance011ce <160853591+Fance011ce@users.noreply.github.com>
2026-06-17 17:03:20 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,11 @@
-import pytest
+import pytest_asyncio
 from httpx import AsyncClient
 from src.main import app
 from src.database import Base, get_db
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
+# Тестовая БД в памяти
 TEST_DATABASE_URL = "sqlite+aiosqlite:///:memory:"
 
 test_engine = create_async_engine(TEST_DATABASE_URL, echo=False)
@@ -14,16 +15,18 @@ async def override_get_db():
     async with TestingSessionLocal() as session:
         yield session
 
-@pytest.fixture(autouse=True)
+@pytest_asyncio.fixture(autouse=True)
 async def setup_db():
+    # Создаем таблицы
     async with test_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     
+    # Подменяем зависимость
     app.dependency_overrides[get_db] = override_get_db
     yield
     app.dependency_overrides.clear()
 
-@pytest.fixture
+@pytest_asyncio.fixture
 async def client():
     async with AsyncClient(app=app, base_url="http://test") as client:
         yield client
